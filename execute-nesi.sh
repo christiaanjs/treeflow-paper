57283da7a69a148ad15cdb0ad2197f3b3e3a850d
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2020-11-26 01:27:05 +1300

    Add workflow to execution script

--- execute-nesi.sh
+++ execute-nesi.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 bash setup/load-modules-nesi.sh
-snakemake -j 100 --cluster-config config/nesi-config.yaml --cluster "sbatch --mem={cluster.mem} --time={cluster.time}"
+snakemake -j 100 --cluster-config config/nesi-config.yaml --cluster "sbatch --mem={cluster.mem} --time={cluster.time}" -s workflow/sim.smk
