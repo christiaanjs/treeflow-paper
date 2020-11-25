a2227df3e984b3d19aaddacce6d399e9ad50e9d1
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2020-11-26 01:29:14 +1300

    Fix typo in module load script

--- setup/load-modules-nesi.sh
+++ setup/load-modules-nesi.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 module load Miniconda3/4.8.3
 module load GCC/9.2.0
-Java/1.8.0_144
+module load Java/1.8.0_144
 module load RAxML/8.2.12-gimkl-2020a
 module load ant/1.10.1-Java-1.8.0_144
 export TREEFLOW_LIB=~/treeflow-lib
